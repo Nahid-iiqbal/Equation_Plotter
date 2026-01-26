45deaee32862eb8e1d18318214e64bec9907fd0a
Nahid Iqbal <2405051@ugrad.cse.buet.ac.bd>
2026-01-27 00:45:51 +0600

    First Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module org.example.equation_plotter {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.equation_plotter to javafx.fxml;
+    exports org.example.equation_plotter;
+}
---
+++ src/main/java/org/example/equation_plotter/HelloController.java
@@ -0,0 +1,9 @@
+package org.example.equation_plotter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+}
