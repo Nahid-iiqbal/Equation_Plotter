e387384c15acae18991cf1acbfb73e2f02737f48
ref000d <rafidmuammar@gmail.com>
2026-01-31 20:40:03 +0600

    [Smol commit] Fixed the parser error message.

--- src/main/java/org/example/equation_plotter/Launcher.java
+++ src/main/java/org/example/equation_plotter/Launcher.java
@@ -1,10 +1,11 @@
 package org.example.equation_plotter;
-
+import org.mariuszgromada.math.mxparser.*;
 import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+        License.iConfirmNonCommercialUse("amichodu");
         Application.launch(Equator.class, args);
     }
 }
-// random comment just for testing git push and pull    
+// random comment just for testing git push and pull
